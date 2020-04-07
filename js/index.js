05a1c0ab1137c98dddeff9133daea354e074e866
Erick Canales <e94canales@gmail.com>
2020-04-06 19:28:46 -0700

    mpv/stretch

--- js/index.js
+++ js/index.js
@@ -43,18 +43,90 @@ const siteContent = {
 const siteTitle = document.querySelector("head title");
 const midImg = document.getElementById("middle-img");
 const ctaText = document.querySelector(".cta-text h1");
+const ctaButton = document.querySelector(".cta-text button");
 const ctaImg = document.getElementById("cta-img");
 const logo = document.getElementById("logo-img");
-const navLinks = document.querySelectorAll("nav a")
-const navTitles = Object.values(siteContent["nav"])
+const navLinks = document.querySelectorAll("nav a");
+const nav = document.querySelector("nav");
+const navTitles = Object.values(siteContent["nav"]);
+const h4s = document.querySelectorAll(".main-content h4");
+const h4Context = document.querySelectorAll(".main-content p");
+const mainKeys = Object.entries(siteContent["main-content"]);
+const contactH4 = document.querySelector(".contact h4");
+const contactPs = document.querySelectorAll(".contact p");
+const footer = document.querySelector("footer p");
 
 // IMAGES ///
-logo.setAttribute('src', siteContent["nav"]["img-src"])
-ctaImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
 // NAV ///
 for (let i = 0; i < navLinks.length; i++){
   navLinks[i].text = navTitles[i]
 }
 
-// CTA //
+// CTA ///
+ctaText.innerHTML = siteContent["cta"]["h1"];
+ctaButton.innerHTML = siteContent["cta"]["button"];
+
+
+
+// MAIN CONTENT ///
+
+// H4 //////////////////
+let h4Titles = [];
+// FILTERS KEYS CONTAINING H4
+for (let i = 0 ; i < mainKeys.length ; i++){
+  if (mainKeys[i][0].includes('h4')){
+    h4Titles.push(mainKeys[i])
+  }
+}
+// SETS H4 VALUES
+for (let i = 0 ; i < h4Titles.length ; i++){
+  h4s[i].innerHTML = h4Titles[i][1]
+}
+
+// H4 CONTENTS ///////////
+let h4Content = [];
+for (let i = 0 ; i < mainKeys.length ; i++){
+  if (mainKeys[i][0].includes("content")){
+    h4Content.push(mainKeys[i][1])
+  }
+}
+for (let i = 0 ; i < h4Content.length ; i++){
+  h4Context[i].innerHTML = h4Content[i];
+}
+
+// CONTACT /////////
+let contP = Object.values(siteContent["contact"])
+contactH4.innerHTML = siteContent["contact"]["contact-h4"]
+for (let i = 0 ; i < contactPs.length ; i++){
+  contactPs[i].innerHTML = contP[i + 1]
+}
+
+// FOOTER ////////
+footer.innerHTML = siteContent["footer"]["copyright"];
+
+
+// STRETCHH ///
+
+
+
+let newAnchor = document.createElement("a");
+newAnchor.href = "#";
+newAnchor.text = "Sign Up!"
+
+let newAnchor2 = document.createElement("a");
+newAnchor2.href = "#";
+newAnchor2.text = "Login";
+
+nav.appendChild(newAnchor);
+nav.appendChild(newAnchor2);
+
+for (let i = 0 ; i < navLinks.length ; i++){
+  navLinks[i].style.color = "green"
+  navLinks[i].style.fontSize = "17px"
+  navLinks[i].style.textDecoration = "none"
+  navLinks[i].setAttribute("onmouseover", "this.style.color = 'red'")
+}
